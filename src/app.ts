447bec026df1cb4ae56ed86cf9b9fd9b530ede97
Meenashi <jeyanthi_sm@yahoo.com>
2023-03-04 22:10:12 +0000

    Refactoring API to MVC and introducing a seperation of concerns

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 export const app = express();
+import { router } from './routes/routes';
+
 app.use(express.json());
 app.get('/', (req, res) => res.send('Welcome to the DrinksAPI!'));
 app.get('/coffeelover', (req, res) => res.send('I like coffee!'));
-const coffeeType = {drinkType:'Coffee', name:'Latte'}
-app.get('/coffee', (req, res) => res.json(coffeeType));
+
+app.use('/', router);
+
